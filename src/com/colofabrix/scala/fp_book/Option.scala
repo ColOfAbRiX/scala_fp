c94249c9d8c67a70a330114d0ea33401ebcac2e0
Fabrizio Colonna <colofabrix@tin.it>
2016-02-02 22:45:24 +0000

    Option

---
+++ src/com/colofabrix/scala/fp_book/Option.scala
@@ -0,0 +1,27 @@
+/**
+  * Functional Programming in Scala
+  * P. Chiusano, R. Bjarnason
+  * Manning Edition
+  *
+  * Exercises solved by Fabrizio Colonna
+  *
+  * Chapter 4
+  */
+
+package com.colofabrix.scala.fp_book
+
+trait Option[+A] {
+
+  // --- Listing 4.2 --- //
+
+  def map[B]( f: A => B ): Option[A]
+
+  def flatMap[B]( f: A => Option[B] ): Option[B]
+
+  def getOrElse[B >: A]( default: => B): B
+
+  def orElse[B >: A]( ob: => Option[B] ): Option[B]
+
+  def filter( f: A => Boolean ): Option[A]
+
+}
